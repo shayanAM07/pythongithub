88a2954715a0803806e0e0c928abe695e534dbe4
shayanAM07 <shayanhos86@gmail.com>
2024-10-16 18:14:26 +0330

    Update coin.py

--- coin.py
+++ coin.py
@@ -3,4 +3,14 @@ from tkinter import messagebox
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+def coin():
+    cointosstime= int(Entry.get())
+    except ValueError as e:
+    messagebox.showerror("error,error")
+    return
 
+
+
+        
+    
+    
