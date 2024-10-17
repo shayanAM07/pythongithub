5cbe9a8a3807923cdbd284b629d5eac56b801ace
shayanAM07 <shayanhos86@gmail.com>
2024-10-17 22:08:10 +0330

    Update coin.py

--- coin.py
+++ coin.py
@@ -3,23 +3,50 @@ from tkinter import messagebox
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 def coin():
-    cointosstime= int(Entry.get())
-
+    try:
+       cointosstime = int(entry.get())
     except ValueError as e:
-    messagebox.showerror("error,error")
-    return
+      messagebox.showerror("error,error")
+      return
 
-    result= np.random.choice(["khat","Shir"], size=cointosstime)
+    result = np.random.choice(["Khat", "Shir"], size=cointosstime)
 
-    khat_count= np.sum(result== "Khat")
-    shir_count= np.sum(result== "Shir")
+    khat_count= np.sum(result == "Khat")
+    shir_count= np.sum(result == "Shir")
 
-    prob_khat= khat_count / cointosstime
-    prob_shir= shir_count / cointosstime
+    prob_khat = khat_count / cointosstime
+    prob_shir = shir_count / cointosstime
 
-    result_label.config(text=f"Shir:{shir_count}\n khat:{khat_count}")
+    result_label.config(text=f"Shir:{shir_count}\nkhat:{khat_count}")
 
-    pro_label.config(text=f"")
+    pro_label.config(text=f"prob of the shir:{prob_shir:.2f}\nprob of the khat:{prob_khat:.2f}")
+
+    fig, ax= plt.subplots()
+    ax.bar(["Shir", "Khat"], [shir_count, khat_count], color=['blue', 'orange'])
+    canvas= FigureCanvasTkAgg(fig,master=window)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+window = tk.Tk()
+window.title("coin")
+window.geometry("400x500")
+
+lal=tk.Label(text="tedad partabe seke")
+lal.pack()
+entry=tk.Entry(window)
+entry.pack()
+
+btn=tk.Button(window,text="Start",command=coin)
+btn.pack()
+
+result_label=(tk.Label(text=""))
+result_label.pack()
+
+pro_label=(tk.Label(text=""))
+pro_label.pack()
+
+window.mainloop()
 
     
